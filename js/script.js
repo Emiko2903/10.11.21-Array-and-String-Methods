a44449fd0edc04d3717ff7dc81202bf5bbc6dd44
Emiko2903 <seaman2903@mail.ru>
2021-11-10 01:02:28 +0400

    Compeated Task Arr and String

---
+++ js/script.js
@@ -0,0 +1,90 @@
+
+
+
+// //       task - 1 (start)
+
+
+// function upperLowerCase (x) {
+//     let y = x.toUpperCase();
+//     let z = x.toLowerCase();
+//     if (x == y || x == z) {
+//         return true
+//     } else {
+//         return false
+//     }
+    
+// }
+
+
+// document.write(upperLowerCase("Azerbaijan"));
+
+
+// //       task - 1 (end)
+
+
+
+
+// //       task - 2 (start)
+
+
+
+// function arrayJoin (x,y) {
+    
+//      if (x.length == 1) {
+//     } else if (x.length == 2) {
+//         x.splice(1,0, `${y}`);
+//     } else if (x.length == 3) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//     } else if (x.length == 4) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);
+//     } else if (x.length == 5) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//     } else if (x.length == 6) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//     } else if (x.length == 7) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//         x.splice(9,0, `${y}`);  
+//     } else if (x.length == 8) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//         x.splice(9,0, `${y}`);  
+//         x.splice(11,0, `${y}`);  
+//     } else if (x.length == 9) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//         x.splice(9,0, `${y}`);  
+//         x.splice(11,0, `${y}`);  
+//         x.splice(13,0, `${y}`);  
+//     } else if (x.length == 10) {
+//         x.splice(1,0, `${y}`);
+//         x.splice(3,0, `${y}`);
+//         x.splice(5,0, `${y}`);  
+//         x.splice(7,0, `${y}`);  
+//         x.splice(9,0, `${y}`);  
+//         x.splice(11,0, `${y}`);  
+//         x.splice(13,0, `${y}`);  
+//         x.splice(15,0, `${y}`);  
+//     }
+//     return x;
+// }
+
+// document.write(arrayJoin(["Pepsi","Sprite","Coca-cola","Ice-tea",], "//"));
+
+
+// //       task - 2 (end)
